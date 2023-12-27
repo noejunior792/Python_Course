8b497640e928b02640ad86696516e56ba827e74e
Noé Júnior <153394025+noejunior792@users.noreply.github.com>
2023-12-27 12:54:13 +0100

    Add files via upload

---
+++ Aula05/aula05.py
@@ -0,0 +1,18 @@
+import random
+
+num_i=10
+num_f=5.2
+num_c=1j
+
+mu1=[ #list
+    random.randrange(0,60),
+    random.randrange(0,60),
+    random.randrange(0,60),
+    random.randrange(0,60),
+    random.randrange(0,60)
+    ]
+
+
+print("Valor: ", num_c, "Tipo: " , type(num_c))
+
+print(mu1)
